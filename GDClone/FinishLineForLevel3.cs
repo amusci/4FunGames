01b3d78ea93b5437aee48a8665205d2a0c63c1a8
Antonio Musciano <71357098+frky1@users.noreply.github.com>
2024-04-29 19:06:26 -0400

    to go to other levels

---
+++ GDClone/FinishLineForLevel3.cs
@@ -0,0 +1,28 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class FinishLineForLevel3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            SceneManager.LoadScene("end");
+            
+        }
+        
+    }
+}
