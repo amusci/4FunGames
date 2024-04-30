58e01d49e87da90851993a0fe0599a499d76472f
Antonio Musciano <71357098+frky1@users.noreply.github.com>
2024-04-29 20:38:30 -0400

    scroll den

---
+++ GDClone/bgscroll.cs
@@ -0,0 +1,27 @@
+using System.Threading;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+
+    public float scrollSpeed = 0.5f;
+
+    private float offset;
+    private Material mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        mat = GetComponent<Renderer>().material;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        offset += (Time.deltaTime * scrollSpeed) / 10f;
+        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+
+    }
+}
