1ded47e806f004b329acbf959375be7a320c4800
Antonio Musciano <71357098+frky1@users.noreply.github.com>
2024-05-21 19:09:12 -0400

    adding jump and jump animations

--- Godot/playground/player.gd
+++ Godot/playground/player.gd
@@ -4,8 +4,13 @@ extends CharacterBody2D
 const GRAVITY : int = 1000
 # setting gravity constant
 const SPEED : int = 300
+# setting speed constant
+const JUMPHEIGHT : int = -500
+# setting jumpheight constant
+const JUMPSPEED : int = 300
+# setting jumpspeed constant
 
-enum State { idle, run } 
+enum State { idle, run, jump} 
 # amount of states that the player can be in
 
 var current_state = State.idle
@@ -17,9 +22,11 @@ func _ready():
 
 func _physics_process(delta):
 	# Function called every physics frame.
+	print("Current State: ", State.keys()[current_state])
 	player_falling(delta)
 	player_idle(delta)
 	player_run(delta)
+	player_jump(delta)
 	
 	move_and_slide()
 	
@@ -36,7 +43,11 @@ func player_idle(delta):
 	if is_on_floor():
 		current_state = State.idle
 		
+		
 func player_run(delta):
+	if !is_on_floor():
+		# if jumping / falling, we dont want our run to be called
+		return
 	# Function handles player run
 	var direction = Input.get_axis("move_left","move_right") # get left and right inputs
 	print(direction) # left = -1, right = 1, nothing = 0
@@ -47,16 +58,32 @@ func player_run(delta):
 		
 	if direction != 0: # if we are moving
 		current_state = State.run # change state to run
+		
 		if direction > 0: # if direction is right
 			animated_sprite_2d.flip_h = false # no need to touch sprite
 		else: # if direction is left
 			animated_sprite_2d.flip_h = true # flip sprite
-
+			
+func player_jump(delta):
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMPHEIGHT
+		current_state = State.jump
+		
+	
+	if !is_on_floor() and current_state == State.jump:
+		var direction = Input.get_axis("move_left","move_right") # get left and right inputs
+		velocity.x += direction * JUMPSPEED * delta
+		
 func player_animations():
 	# animations depending on state
 	if current_state == State.idle:
 		animated_sprite_2d.play("idle")
-	elif current_state == State.run:
+	elif current_state == State.run and is_on_floor():
+		# make sure that x axis movement in air stays as jump state
 		animated_sprite_2d.play("player_run")
+	elif current_state == State.jump:
+		animated_sprite_2d.play("player_jump")
+	
 		
 		
