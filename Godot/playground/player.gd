2708a15102d865c01746a8c4dededd4d7d298ae6
Antonio Musciano <71357098+frky1@users.noreply.github.com>
2024-05-21 19:44:52 -0400

    coding problem solving and Goat Life - JDubz

--- Godot/playground/player.gd
+++ Godot/playground/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d = $AnimatedSprite2D # reference to AnimatedSprite2D node
+@onready var slash_timer = $SlashTimer # reference to the Timer node
 
 const GRAVITY : int = 1000
 # setting gravity constant
@@ -10,7 +11,7 @@ const JUMPHEIGHT : int = -500
 const JUMPSPEED : int = 300
 # setting jumpspeed constant
 
-enum State { idle, run, jump} 
+enum State {idle,run,jump,slash} 
 # amount of states that the player can be in
 
 var current_state = State.idle
@@ -27,6 +28,7 @@ func _physics_process(delta):
 	player_idle(delta)
 	player_run(delta)
 	player_jump(delta)
+	player_slash(delta)
 	
 	move_and_slide()
 	
@@ -34,20 +36,21 @@ func _physics_process(delta):
 
 func player_falling(delta):
 	# Gravity function
-	if !is_on_floor():
-		velocity.y += GRAVITY * delta
+	if !is_on_floor(): # if not touching floor
+		velocity.y += GRAVITY * delta # apply gravity
 		
 		
 func player_idle(delta):
 	# Function to set the state of the player to idle
-	if is_on_floor():
-		current_state = State.idle
+	if is_on_floor() and current_state != State.slash: # if we are just chilling
+		current_state = State.idle # idle
 		
 		
 func player_run(delta):
 	if !is_on_floor():
 		# if jumping / falling, we dont want our run to be called
 		return
+	
 	# Function handles player run
 	var direction = Input.get_axis("move_left","move_right") # get left and right inputs
 	print(direction) # left = -1, right = 1, nothing = 0
@@ -65,16 +68,33 @@ func player_run(delta):
 			animated_sprite_2d.flip_h = true # flip sprite
 			
 func player_jump(delta):
-	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMPHEIGHT
-		current_state = State.jump
+	# this function handles player jump
+	if Input.is_action_just_pressed("jump") and is_on_floor(): # if we jumped on the floor
+		velocity.y = JUMPHEIGHT # jump
+		current_state = State.jump # jump state
 		
-	
-	if !is_on_floor() and current_state == State.jump:
+	if !is_on_floor() and current_state == State.jump: # if we are jumping
 		var direction = Input.get_axis("move_left","move_right") # get left and right inputs
-		velocity.x += direction * JUMPSPEED * delta
+		velocity.x += direction * JUMPSPEED * delta # a little spice
+
+func player_slash(delta):
+	# this function handles player slash
+	var direction = Input.get_axis("move_left","move_right")
+	if direction != 0: # if we are moving
+		return # hell no slash
+	
+	if Input.is_action_just_pressed("slash") and is_on_floor(): # if we on the floor and want to slash
+		current_state = State.slash # of course we slash
+		slash_timer.start() # start timer because idk another way atm
 		
+
+func _on_slash_timer_timeout():
+	# this handles slash animation timer ending
+	if current_state == State.slash: # if the timer is ending on slash state
+		current_state = State.idle # switch it to idle after
+
+
+	
 func player_animations():
 	# animations depending on state
 	if current_state == State.idle:
@@ -84,6 +104,6 @@ func player_animations():
 		animated_sprite_2d.play("player_run")
 	elif current_state == State.jump:
 		animated_sprite_2d.play("player_jump")
-	
-		
-		
+	elif current_state == State.slash:
+		animated_sprite_2d.play("player_slash")
+
