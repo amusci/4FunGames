101337c348411a4ae0987b8596b5ad3bca4b5781
Antonio Musciano <71357098+frky1@users.noreply.github.com>
2024-05-21 16:24:04 -0400

    left and right movements

--- Godot/playground/player.gd
+++ Godot/playground/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const GRAVITY : int = 1000
 # setting gravity constant
+const SPEED : int = 300
 
 enum State { idle, run } 
 # amount of states that the player can be in
@@ -17,6 +18,7 @@ func _physics_process(delta):
 	# Function called every physics frame.
 	player_falling(delta)
 	player_idle(delta)
+	player_run(delta)
 	
 	move_and_slide()
 
@@ -30,3 +32,11 @@ func player_idle(delta):
 	# Function to set the state of the player to idle
 	if is_on_floor():
 		current_state = State.idle
+		
+func player_run(delta):
+	var direction = Input.get_axis("move_left","move_right")
+	
+	if direction: # if there's left and right input
+		velocity.x = direction * SPEED # multiply speed to that direction
+	else: # if there's no input
+		velocity.x = move_toward(velocity.x, 0, SPEED) # move towards 0 
