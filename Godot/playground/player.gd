e2058612f4c6814b6471de8c58fc6350c76c978b
Antonio Musciano <71357098+frky1@users.noreply.github.com>
2024-05-21 16:42:13 -0400

    add run animations, flipping animations
    https://youtu.be/BngLl1GsYOU?list=PLWTXKdBN8RZdvd3bbCC4mg2kHo3NNnBz7

--- Godot/playground/player.gd
+++ Godot/playground/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+@onready var animated_sprite_2d = $AnimatedSprite2D # reference to AnimatedSprite2D node
 
 const GRAVITY : int = 1000
 # setting gravity constant
@@ -21,6 +22,8 @@ func _physics_process(delta):
 	player_run(delta)
 	
 	move_and_slide()
+	
+	player_animations()
 
 func player_falling(delta):
 	# Gravity function
@@ -34,9 +37,26 @@ func player_idle(delta):
 		current_state = State.idle
 		
 func player_run(delta):
-	var direction = Input.get_axis("move_left","move_right")
-	
+	# Function handles player run
+	var direction = Input.get_axis("move_left","move_right") # get left and right inputs
+	print(direction) # left = -1, right = 1, nothing = 0
 	if direction: # if there's left and right input
 		velocity.x = direction * SPEED # multiply speed to that direction
 	else: # if there's no input
 		velocity.x = move_toward(velocity.x, 0, SPEED) # move towards 0 
+		
+	if direction != 0: # if we are moving
+		current_state = State.run # change state to run
+		if direction > 0: # if direction is right
+			animated_sprite_2d.flip_h = false # no need to touch sprite
+		else: # if direction is left
+			animated_sprite_2d.flip_h = true # flip sprite
+
+func player_animations():
+	# animations depending on state
+	if current_state == State.idle:
+		animated_sprite_2d.play("idle")
+	elif current_state == State.run:
+		animated_sprite_2d.play("player_run")
+		
+		
