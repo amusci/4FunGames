45de6c806488572cda8e84f19aa9014f3abc01e5
Antonio Musciano <71357098+frky1@users.noreply.github.com>
2024-05-23 15:27:43 -0400

    coyote time basic movement
    https://youtu.be/c5msTt9Jeyg

--- Godot/playground/PixelPlatformer/Player/player.gd
+++ Godot/playground/PixelPlatformer/Player/player.gd
@@ -4,23 +4,38 @@ extends CharacterBody2D
 @export var speed = 300.0
 @export var jump_force = -250.0
 @export var jump_time : float = 0.25
+@export var coyote_time : float = 0.075
+@export var gravity_multiplier : float = 3.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = 980
+var is_jumping : bool = false
+var jump_timer : float = 0
+var coyote_timer : float = 0
 
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if not is_on_floor() and not is_jumping:
+		velocity.y += gravity * gravity_multiplier * delta
+		coyote_timer += delta
+	else:
+		coyote_timer = 0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote_timer < coyote_time):
 		velocity.y = jump_force
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
+		is_jumping = true
+	elif Input.is_action_pressed("jump") and is_jumping:
+		velocity.y = jump_force
+		
+	if is_jumping and Input.is_action_pressed("jump") and jump_timer < jump_time:
+		jump_timer += delta
+	else:
+		is_jumping = false
+		jump_timer = 0
+		
+		
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * speed
 	else:
