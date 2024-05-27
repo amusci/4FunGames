b802e58536da1e344cd6b43099f58089c3f933ea
Antonio Musciano <71357098+frky1@users.noreply.github.com>
2024-05-27 16:08:04 -0400

    main menu able to quit

---
+++ Godot/playground/UI/main_menu.gd
@@ -0,0 +1,16 @@
+class_name MainMenu
+extends Control
+
+
+
+func _on_play_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_settings_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	# This function when pressed quits the game
+	get_tree().quit()
