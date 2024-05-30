cb0f850591ac4c447b835410f2a757053c0983cd
Antonio Musciano <71357098+frky1@users.noreply.github.com>
2024-05-29 22:48:21 -0400

    calm before the storm

--- Godot/playground/PixelPlatformer/Player/player.gd
+++ Godot/playground/PixelPlatformer/Player/player.gd
@@ -1,5 +1,4 @@
 class_name Player
-
 extends CharacterBody2D
 
 # Export variables
@@ -19,7 +18,6 @@ var coyote_counter : float = 0
 var jump_buffer_counter : float = 0
 var can_control : bool = true
 
-
 # On ready variables
 @onready var sprite_2d = $Sprite2D
 @onready var animation_player = $AnimationPlayer
@@ -28,10 +26,9 @@ func _physics_process(delta):
 	if not can_control: # If we cant control our player, return
 		return
 	# Function called once per physics frame
-	player_jump(delta)
-	player_run(delta)
-	move_and_slide()
-
+	player_jump(delta) # Line 33
+	player_run(delta) # Line 47
+	move_and_slide() # 
 
 func player_run(delta):
 	var direction = Input.get_axis("move_left", "move_right") # Get left and right inputs
@@ -45,7 +42,7 @@ func player_run(delta):
 			sprite_2d.flip_h = false
 	else: # If no inputs
 		velocity.x = move_toward(velocity.x, 0, acceleration + 3) # MOVE TO A HALT
-	player_animations(direction) # call animations
+	player_animations(direction) # Call animations
 
 func player_jump(delta):
 	# Print debug information
@@ -55,19 +52,18 @@ func player_jump(delta):
 	print("jump_timer: ", jump_timer)
 	
 	# This function handles player's ability to jump
-	
 	if is_on_floor(): # if we are on the floor
-		coyote_counter = coyote_time # set the counter to the time 
-	else: # if we aren't on the floor (falling)
+		coyote_counter = coyote_time # Set the counter to the time 
+	else: # If we aren't on the floor (falling)
 		velocity.y += gravity * gravity_multiplier * delta # Apply gravity with multiplier
-		coyote_counter -= delta # decrement counterq
+		coyote_counter -= delta # Decrement counterq
 		
 	if Input.is_action_just_pressed("jump"):
 		# This allows us to press jump slightly before we land for smooth controls
 		jump_buffer_counter = jump_buffer_time # We set the counter to the time we want our character to jump before landing
 	else:
 		jump_buffer_counter -= delta # Decrement the counter by delta since we aren't jumping we want it to go to 0
-	if coyote_counter >= 0 and jump_buffer_counter > 0: # if we arent jumping (since 0) and jumping (line 64) 
+	if coyote_counter >= 0 and jump_buffer_counter > 0: # If we arent jumping (since 0) and jumping (line 64) 
 		jump_buffer_counter = 0 # Instantly set counter back to 0
 		velocity.y = jump_force # Apply jump force
 		is_jumping = true # Switch jump flag to true
@@ -89,7 +85,7 @@ func handle_death() -> void:
 	
 func reset_player() -> void:
 	# Function handles player reset
-	global_position = Vector2(0,0)  # Set the global position to (0, 0)
+	global_position = Vector2(0,0) # Set the global position to (0, 0)
 	visible = true
 	can_control = true
 	
@@ -101,4 +97,3 @@ func player_animations(direction : float) -> void:
 		animation_player.play("jump") # Jumping animation
 	else: 
 		animation_player.play("idle") # Idle animation
-	
