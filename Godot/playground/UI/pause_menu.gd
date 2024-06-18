a62e7c3646226ae5f6edfdc5203c7b3f5792ecea
Antonio Musciano <antoniomusciano@gmail.com>
2024-06-18 17:30:07 -0400

    pausing but menu wont show up

---
+++ Godot/playground/UI/pause_menu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+func _ready():
+	$AnimationPlayer.play("RESET")
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+
+func testEsc():
+	if Input.is_action_just_pressed("pause") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("pause") and get_tree().paused:
+		resume()
+
+
+func _on_resume_pressed():
+	resume()
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _process(delta):
+	testEsc()
+
+
+func _on_options_pressed():
+	pass
