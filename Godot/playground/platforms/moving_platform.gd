f51dfd8a43752379ea748187ec3e2d4afadfaa37
Antonio Musciano <antoniomusciano@gmail.com>
2024-06-25 16:35:21 -0400

    lil documentation for da noobs
    https://youtu.be/iwNQ2sqH2cw?list=PLeAs1IbNsvjxUYaRT_K8l66NlxOWCU3UJ

--- Godot/playground/platforms/moving_platform.gd
+++ Godot/playground/platforms/moving_platform.gd
@@ -1,18 +1,36 @@
 extends Path2D
 
+'''
+
+A NOTE FROM THE GOAT!
+
+If you want the platform to move in a closed path (a shape or something)
+turn LOOP ON in the inspector.
+
+If the platform is moving in an unclosed path (a line or something) turn Loop OFF.
+
+'''
+
+# Inspector Variables
+
 @export var loop = true
 @export var speed = 2.0
 @export var speed_scale = 1.0
+
+# Get the specific nodes
 
 @onready var path = $PathFollow2D
 @onready var animation = $AnimationPlayer
 
 
 func _ready():
+	
+	# Make sure animation plays
 	if not loop:
 		animation.play("move_left_to_right")
 		animation.speed_scale = speed_scale
 		set_process(false)
 
 func _physics_process(delta):
+	# Make sure progress increments
 	path.progress += speed
