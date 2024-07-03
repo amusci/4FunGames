5edb3281d8c88ed5a0d41b5c94849c3bf3470bb7
Antonio Musciano <antoniomusciano@gmail.com>
2024-07-03 17:15:16 -0400

    made the canon make sense
    # 0 = left
    # 45 = down left
    # 90 = down
    # 135 = down right
    # 180 = left
    # 225 = up left
    # 270 = up
    # 315 = top right
    # 360 = left
    # USE THIS TO CREATE YOUR OWN ANGLES. ANY NUMBER BETWEEN 0-360 IS USABLE

--- Godot/playground/obstacles/Cannon.gd
+++ Godot/playground/obstacles/Cannon.gd
@@ -3,7 +3,18 @@ extends Node2D
 
 @export var level: String
 @export var rotation_of_cannon: int = 45
-@export var direction_of_bullet : int = 45
+@export var direction_of_bullet : int = 335
+# 0 = left
+# 45 = down left
+# 90 = down
+# 135 = down right
+# 180 = left
+# 225 = up left
+# 270 = up
+# 315 = top right
+# 360 = left
+# USE THIS TO CREATE YOUR OWN ANGLES. ANY NUMBER BETWEEN 0-360 IS USABLE
+
 
 @onready var main = get_tree().get_root().get_node(level)
 @onready var projectile = load("res://obstacles/egg_projectile.tscn")
@@ -11,11 +22,15 @@ extends Node2D
 func _physics_process(delta):
 	#rotation_degrees += rotation_of_cannon * delta # This would make the cannon rotate
 	pass
+	
+func angle_to_vector(angle_deg):
+	var angle_rad = deg_to_rad(angle_deg)
+	return Vector2(cos(angle_rad), sin(angle_rad)).normalized()
 
 func shoot():
 	var proj = projectile.instantiate() # Allow us to use our projectile
-	proj.dir = direction_of_bullet # Direction of projectile
-	proj.spawnPos = global_position + Vector2(0,-5) # Spawn position
+	proj.dir = angle_to_vector(direction_of_bullet)
+	proj.spawnPos = global_position + Vector2(0,0) # Spawn position
 	proj.spawnRot = rotation # Set rotation of projectile
 	#proj.zdex = z_index - 1 # Might do something for this idk
 	main.add_child.call_deferred(proj) # Spawn in the projectile
