db2b17af35807250108ae76f9cd02b6f649861c6
amusci <71357098+amusci@users.noreply.github.com>
2024-07-08 18:26:03 -0400

    set up cinematic + fake level 3

---
+++ Godot/playground/levels/level_cinematics/cine_script.gd
@@ -0,0 +1,28 @@
+extends Node
+
+@onready var cine_player = get_tree().get_root().find_child("cinematic_player", true, false)
+@onready var timer_label = get_tree().get_root().find_child("timer_label", true, false)
+
+var space_hold_time = 0.0  # Timer to track spacebar hold duration
+
+@export var skip_time : int = 3
+
+func _ready():
+	cine_player.play("opening_cine")
+	
+
+func _physics_process(delta):
+	timer_label.set_text("Press Spacebar to Skip Cinematic: " + str(round(space_hold_time)))
+	if Input.is_action_pressed("jump"): # If spacebar is held
+		space_hold_time += delta # Increment Timer
+		print(space_hold_time)
+		   
+		if space_hold_time >= skip_time:
+			TransitionScreen.transition()
+			await get_tree().create_timer(.5).timeout
+			print("Changing scene to level 3")
+			get_tree().change_scene_to_file("res://levels/level_three.tscn")
+		
+			space_hold_time = 0.0  # Reset the timer after printing
+	else:
+		space_hold_time = 0.0  # Reset the timer if the spacebar is released
