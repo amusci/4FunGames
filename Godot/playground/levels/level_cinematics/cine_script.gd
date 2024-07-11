fd7cdad20e691bc25df0ed4befd1ebd9cbe58270
amusci <71357098+amusci@users.noreply.github.com>
2024-07-10 22:01:31 -0400

    did a bit of level 1

--- Godot/playground/levels/level_cinematics/cine_script.gd
+++ Godot/playground/levels/level_cinematics/cine_script.gd
@@ -4,26 +4,34 @@ extends Node
 @onready var timer_label = get_tree().get_root().find_child("timer_label", true, false)
 
 var space_hold_time = 0.0  # Timer to track spacebar hold duration
+var in_cine_time : int = 0
 
 @export var skip_time : int = 3
+@export var scene_to_send : String
+@export var cine_finish_time : int
 
 func _ready():
 	MusicManager.stop_music() # Start with main menu music off
 	cine_player.play("opening_cine")
-	
+
 
 func _physics_process(delta):
 	timer_label.set_text("HOLD SPACEBAR TO SKIP CINEMATIC: " + str(round(space_hold_time)))
 	if Input.is_action_pressed("jump"): # If spacebar is held
 		space_hold_time += delta # Increment Timer
-		print(space_hold_time)
-		   
+		#print(space_hold_time)
+		
 		if space_hold_time >= skip_time:
 			TransitionScreen.transition()
 			await get_tree().create_timer(.5).timeout
 			print("Changing scene to level 3")
-			get_tree().change_scene_to_file("res://levels/level_three.tscn")
-		
+			get_tree().change_scene_to_file(scene_to_send)
 			space_hold_time = 0.0  # Reset the timer after printing
 	else:
 		space_hold_time = 0.0  # Reset the timer if the spacebar is released
+
+	if in_cine_time >= cine_finish_time:
+		TransitionScreen.transition()
+		await get_tree().create_timer(.5).timeout
+		get_tree().change_scene_to_file(scene_to_send)
+
